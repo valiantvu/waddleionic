ec4438918c954101a92e4413b718352dd2d6bba2
valiantvu <michelle.thi.vu@gmail.com>
2014-11-04 21:54:39 -0800

    (feat) Add footprint elements with fake footprint data

--- www/tabs/footprints/footprints.js
+++ www/tabs/footprints/footprints.js
@@ -1,6 +1,47 @@
 (function(){
 
-var FootprintsController = function ($scope, $state) {	
+var FootprintsController = function ($scope, $state) {
+
+    $scope.footprint = {
+        "user": {
+            "footprintsCount": 13,
+            "fbProfilePicture": "img/bmo.png",
+            "fbToken":"CAAMxNSdb8MMBAKryfnHRdmoUGaZCINm2mjgm3owffItnrHVINZBKCGhCnfJ821iVff0id1SxUeR5IJmF71UnymKAPupROWZCm1DyglsyNwUrC49A9x6UZBkzj5gzdxf6ZA8GvZBCUwHJSorZCIk4rUiH6r1CdIBZAQJZAyXDV9lTZBZAFDKP97TabiLLZBpM7jdjQUYZD",
+            "name":"Michelle Thi Vu",
+            "facebookID":"10203426526517301"
+        },
+        "checkin": {
+            "checkinTime":"2013-09-29T07:45:38.000Z",
+            "caption":"null",
+            "likes":100000,
+            "photoLarge":"img/adventure_time.png",
+            "source":"facebook",
+            "photoSmall":"img/adventure_time.png",
+            "checkinID":"10201337526173598"
+        },
+        "place":{
+            "lat":19.5,
+            "category":"null",
+            "foursquareID":"Cayman Islands",
+            "country":"null",
+            "lng":-80.5,
+            "name":"Cayman Islands"
+        },
+        "comments":null,
+        "$$hashKey":"object:34"
+    };
+    // $scope.getFootprint = function (footprint) {
+    //     $scope.footprint = footprint;
+
+    //     var checkinID = footprint.checkin.checkinID;
+    //     FootprintRequests.openFootprint = footprint;
+
+    //     FootprintRequests.getFootprintInteractions(checkinID)
+    //     .then(function (data) {
+    //         FootprintRequests.currentFootprint = data.data;
+    //         $scope.selectedFootprintInteractions = FootprintRequests.currentFootprint;
+    //     });
+    // };
 
 };
 
