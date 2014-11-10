518843b69f2472c739d8432bfd742c7c8545b57a
valiantvu <michelle.thi.vu@gmail.com>
2014-11-10 00:34:01 -0800

    (feat) Add search bar, add ng-repeat, add multiple fake footprints

--- www/tabs/footprints/footprints.js
+++ www/tabs/footprints/footprints.js
@@ -2,34 +2,64 @@
 
 var FootprintsController = function (MapFactory, FootprintRequests, $scope, $state) {
 
-    $scope.footprint = {
-        "user": {
-            "footprintsCount": 13,
-            "fbProfilePicture": "img/bmo.png",
-            "fbToken":"CAAMxNSdb8MMBAKryfnHRdmoUGaZCINm2mjgm3owffItnrHVINZBKCGhCnfJ821iVff0id1SxUeR5IJmF71UnymKAPupROWZCm1DyglsyNwUrC49A9x6UZBkzj5gzdxf6ZA8GvZBCUwHJSorZCIk4rUiH6r1CdIBZAQJZAyXDV9lTZBZAFDKP97TabiLLZBpM7jdjQUYZD",
-            "name":"Michelle Thi Vu",
-            "facebookID":"10203426526517301"
+    $scope.footprints = [
+        {
+            "user": {
+                "footprintsCount": 13,
+                "fbProfilePicture": "img/bmo.png",
+                "fbToken":"CAAMxNSdb8MMBAKryfnHRdmoUGaZCINm2mjgm3owffItnrHVINZBKCGhCnfJ821iVff0id1SxUeR5IJmF71UnymKAPupROWZCm1DyglsyNwUrC49A9x6UZBkzj5gzdxf6ZA8GvZBCUwHJSorZCIk4rUiH6r1CdIBZAQJZAyXDV9lTZBZAFDKP97TabiLLZBpM7jdjQUYZD",
+                "name":"Michelle Thi Vu",
+                "facebookID":"10203426526517301"
+            },
+            "checkin": {
+                "checkinTime":"2013-09-29T07:45:38.000Z",
+                "caption":"Adventure Time!! #Finn #Jake #Running",
+                "likes":100000,
+                "photoLarge":"img/adventure_time.png",
+                "source":"facebook",
+                "photoSmall":"img/adventure_time.png",
+                "checkinID":"10201337526173598"
+            },
+            "place": {
+                "lat":19.5,
+                "category":"null",
+                "foursquareID":"Cayman Islands",
+                "country":"null",
+                "lng":-80.5,
+                "name":"Cayman Islands"
+            },
+            "comments":null,
+            "$$hashKey":"object:34"
         },
-        "checkin": {
-            "checkinTime":"2013-09-29T07:45:38.000Z",
-            "caption":"Adventure Time!! #Finn #Jake #Running",
-            "likes":100000,
-            "photoLarge":"img/adventure_time.png",
-            "source":"facebook",
-            "photoSmall":"img/adventure_time.png",
-            "checkinID":"10201337526173598"
-        },
-        "place":{
-            "lat":19.5,
-            "category":"null",
-            "foursquareID":"Cayman Islands",
-            "country":"null",
-            "lng":-80.5,
-            "name":"Cayman Islands"
-        },
-        "comments":null,
-        "$$hashKey":"object:34"
-    };
+        {
+            "user": {
+                "footprintsCount": 13,
+                "fbProfilePicture": "img/bmo.png",
+                "fbToken":"CAAMxNSdb8MMBAKryfnHRdmoUGaZCINm2mjgm3owffItnrHVINZBKCGhCnfJ821iVff0id1SxUeR5IJmF71UnymKAPupROWZCm1DyglsyNwUrC49A9x6UZBkzj5gzdxf6ZA8GvZBCUwHJSorZCIk4rUiH6r1CdIBZAQJZAyXDV9lTZBZAFDKP97TabiLLZBpM7jdjQUYZD",
+                "name":"Michelle Thi Vu",
+                "facebookID":"10203426526517301"
+            },
+            "checkin": {
+                "checkinTime":"2013-09-29T07:45:38.000Z",
+                "caption":"Adventure Time!! #Finn #Jake #Running",
+                "likes":100000,
+                "photoLarge":"img/adventure_time.png",
+                "source":"facebook",
+                "photoSmall":"img/adventure_time.png",
+                "checkinID":"10201337526173598"
+            },
+            "place": {
+                "lat":19.5,
+                "category":"null",
+                "foursquareID":"Candy Kingdom",
+                "country":"null",
+                "lng":-80.5,
+                "name":"Candy Kingdom"
+            },
+            "comments":null,
+            "$$hashKey":"object:35"
+        }
+    ];
 
     $scope.addCheckinToBucketList = function (footprint){
       
