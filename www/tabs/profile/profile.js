db85662465af4baf556f330bc606db63a0c7faba
Vishal Reddy <vreddy18@gmail.com>
2014-12-05 14:36:12 -0500

    (feat) Add user profile info to profile tab

--- www/tabs/profile/profile.js
+++ www/tabs/profile/profile.js
@@ -1,10 +1,20 @@
 (function(){
 
-var ProfileController = function ($scope, $state) {	
+var ProfileController = function ($scope, $state, UserRequests) {	
+
+	$scope.getUserInfo = function (userFbID) {
+		UserRequests.getUserInfo(userFbID)
+		.then(function (data) {
+			$scope.userInfo = data.data;
+			console.log(data);
+		})
+	}
+
+	$scope.getUserInfo(window.sessionStorage.userFbID);
 
 };
 
-ProfileController.$inject = ['$scope', '$state']
+ProfileController.$inject = ['$scope', '$state', 'UserRequests']
 
 angular.module('waddle.profile', [])
   .controller('ProfileController', ProfileController);
