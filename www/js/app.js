4b5cfaa839845e1aa582b19c529ecceccf2dbc21
Vishal Reddy <vreddy18@gmail.com>
2015-04-24 18:26:49 -0400

    Change otherwise to fall back to frontpage instead of home

--- www/js/app.js
+++ www/js/app.js
@@ -199,7 +199,7 @@ angular.module('waddle', ['ionic', 'ngCordova', 'waddle.controllers', 'waddle.se
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
+  $urlRouterProvider.otherwise('/');
 
 });
 
